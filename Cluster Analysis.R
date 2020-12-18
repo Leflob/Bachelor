a1a4480f694cc9885c4e2e485ee2449569404dc2
Leflob <leflobone@gmail.com>
2020-12-18 17:12:12 +0100

    ss

--- Cluster Analysis.R
+++ Cluster Analysis.R
@@ -358,11 +358,140 @@ clust_center = clust.centroid = function(i, dat, clusters) {
 clust_center
 
 
+# Build a kmeans model
+
+#set.seed(0100805)
+###################
+model_km2 <- kmeans(gaming_mean, centers = 5, nstart = 1)
+
+
+# Extract the cluster assignment vector from the kmeans model
+clust_km2 <- print(model_km2)
+
+# Create a new data frame appending the cluster assignment
+library(dplyr)
+gaming_km2 <- mutate(gaming_mean, cluster = model_km2$cluster)
+gaming_km2
+
+#Count 
+count(gaming_km2, cluster)
+
+# Plot the positions of the players and color them using their cluster
+#fviz_cluster(list(data = gaming_mean, cluster = model_km2$cluster))
+
+#Untersuche die Cluster
+
+cl1 = subset(gaming_km2, cluster == "1")
+cl1
+cl2 = subset(gaming_km2, cluster == "2")
+cl2
+cl3 = subset(gaming_km2, cluster == "3")
+cl3
+cl4 = subset(gaming_km2, cluster == "4")
+cl4
+cl5 = subset(gaming_km2, cluster == "5")
+cl5
+
+##############
+
+#1
+social1 = mean(cl1$social)
+functional1 = mean(cl1$functional)
+hedonic1 = mean(cl1$hedonic)
+quality1 = mean(cl1$quality)
+economic1 = mean(cl1$economic)
+identification1 = mean(cl1$identification)
+emotional1 = mean(cl1$emotional)
+
+#2
+social2 = mean(cl2$social)
+functional2 = mean(cl2$functional)
+hedonic2 = mean(cl2$hedonic)
+quality2 = mean(cl2$quality)
+economic2 = mean(cl2$economic)
+identification2 = mean(cl2$identification)
+emotional2 = mean(cl2$emotional)
+
+#3
+social3 = mean(cl3$social)
+functional3 = mean(cl3$functional)
+hedonic3 = mean(cl3$hedonic)
+quality3 = mean(cl3$quality)
+economic3 = mean(cl3$economic)
+identification3 = mean(cl3$identification)
+emotional3 = mean(cl3$emotional)
+
+#4
+social4 = mean(cl4$social)
+functional4 = mean(cl4$functional)
+hedonic4 = mean(cl4$hedonic)
+quality4 = mean(cl4$quality)
+economic4 = mean(cl4$economic)
+identification4 = mean(cl4$identification)
+emotional4 = mean(cl4$emotional)
+
+#5
+social5 = mean(cl5$social)
+functional5 = mean(cl5$functional)
+hedonic5 = mean(cl5$hedonic)
+quality5 = mean(cl5$quality)
+economic5 = mean(cl5$economic)
+identification5 = mean(cl5$identification)
+emotional5 = mean(cl5$emotional)
+#################
+
+#ich muss die CLuster Zentren heruasfinden und dann jeweils den Abstand
+#der mean der Kategorien zu dem jeweiligen zentrum berechnen
+
+#Zusätzlich Clustert R immer nur paarweise. In dem Programm der Studie oder des Buches
+#ist aber aus dem Dendogram ersichtlich, dass hier auch 3 Untersuchungen in einem Cluster
+#stecken können. -> Wie kann man das in R?
+
+
+#####
+#Spiderweb-plot
+############
+
+#install.packages("fmsb")
+library(fmsb)
+
+
+spider_web = data.frame(
+  social = c(5, 0, social1, social2, social3,social4,social5),
+  functional = c(5, 0, functional1, functional2, functional3, functional4, functional5),
+  hedonic = c(5,0, hedonic1, hedonic2, hedonic3, hedonic4, hedonic5),
+  quality = c(5,0, quality1, quality2, quality3, quality4, quality5),
+  economic = c(5,0, economic1, economic2, economic3, economic4, economic5),
+  identification = c(5,0, identification1, identification2, identification3, identification4, identification5),
+  emotional = c(5,0, emotional1, emotional2, emotional3, emotional4, emotional5),
+  row.names = c("max", "min", "Cl1", "CL2", "CL3", "CL4", "CL5")
+)
 
 
 
+spider_web
 
+# Define fill colors
+colors_fill = c(scales::alpha("gray", 0.1),
+                scales::alpha("gold", 0.1),
+                scales::alpha("tomato", 0.2),
+                scales::alpha("skyblue", 0.2),
+                scales::alpha("green", 0.1)
+)
 
+# Define line colors
+colors_line = c(scales::alpha("darkgray", 0.9),
+                scales::alpha("gold", 0.9),
+                scales::alpha("tomato", 0.9),
+                scales::alpha("royalblue", 0.9),
+                scales::alpha("green", 0.9)
+)
 
-
-
+# Create plot
+radarchart(spider_web, 
+           seg = 5,  # Number of axis segments
+           title = "Cluster comparison",
+           pcol = colors_line,
+           pfcol = colors_fill,
+           plwd = 4)
+###################
